c45b5a78af1104079ae6932b8e28f599d83d0b07
ARUN-YERRAM <arunyerram12022005@gmail.com>
2025-01-31 23:56:20 +0530

    Rebuild bcrypt for Render

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const getFields = multer();
 const app = express();
 
